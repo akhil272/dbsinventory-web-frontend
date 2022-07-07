cbfa2f955be519df0aa29e5325e7fdfaf520aa84
akhil272 <akhilms272@gmail.com>
2022-07-07 23:40:19 +0530

    added email verifcation screen

---
+++ pages/auth/confirm-mail/confirm-mail.tsx
@@ -0,0 +1,65 @@
+import LoadingAnimation from "@Components/LoadingAnimation";
+import dbsServer from "@Pages/api/dbsServer";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const ConfirmMail = () => {
+  const [loading, setLoading] = useState(false);
+  const [IsVerified, setIsVerified] = useState(false);
+  const [alreadyVerified, setAlreadyVerified] = useState(false);
+  const router = useRouter();
+  const {
+    query: { token },
+  } = router;
+  useEffect(() => {
+    setLoading(true);
+    if (router.isReady) {
+      const sendToken = async () => {
+        await dbsServer
+          .post("/mail/confirm", { token })
+          .then(() => {
+            setLoading(false);
+            setIsVerified(true);
+            return toast.success("Mail ID verified");
+          })
+          .catch((error) => {
+            setLoading(false);
+            setIsVerified(false);
+            if (error.response.data.error === "Conflict") {
+              setAlreadyVerified(true);
+              return toast.warn("Email address already verified");
+            } else {
+              return toast.error(`${error.response.data.message}`);
+            }
+          });
+      };
+      sendToken();
+    }
+  }, [router.isReady]);
+  if (loading) return <LoadingAnimation message="Please wait" />;
+
+  return (
+    <div className="pb-4">
+      <div className="items-center justify-center flex  ">
+        <img
+          className="object-contain rounded-xl"
+          src={
+            IsVerified
+              ? "/images/Success_Verification_Art.png"
+              : "/images/Failed_Verification_Art.png"
+          }
+        />
+      </div>
+      <div className="mt-2">
+        <h1 className="font-bold text-2xl pb-4">
+          {IsVerified && "Email address verified successfully."}
+          {alreadyVerified && "Email address already verified."}
+          {!IsVerified && !alreadyVerified && "Failed to verify email."}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmMail;
