8955e41de509beb26bbd0dab0f19623de9369ba5
akhil272 <akhilms272@gmail.com>
2022-08-09 06:16:30 +0530

    updated view port

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,28 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <ToastContainer autoClose={2000} transition={Slide} />
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="full-screen" content="yes" />
+        <meta name="browsermode" content="application" />
+        <meta name="screen-orientation" content="portrait" />
+      </Head>
+      <Provider store={store}>
+        <Layout>
+          <ToastContainer autoClose={2000} transition={Slide} />
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
 
